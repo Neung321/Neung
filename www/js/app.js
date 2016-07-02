95e07aecff89374dd02406964ec80edc2d22aacf
Neung321 <bancha.pimsari@gmail.com>
2016-07-02 14:11:27 +0700

    final

--- www/js/app.js
+++ www/js/app.js
@@ -6,19 +6,47 @@
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
+
+
+.controller('MyController', ['$scope', function($scope) {
+
+    $scope.intRedScore = 0;
+    $scope.intBlueScore = 0;
+
+    $scope.btnRedClick = function(redScore) {
+        $scope.intRedScore = redScore + 1;
+
+    }
+    $scope.btnBlueClick = function(blueScore) {
+        $scope.intBlueScore = blueScore + 1;
+
+    }
+
+
+    $scope.btnReset = function() {
+
+        $scope.intRedScore = 0;
+        $scope.intBlueScore = 0;
+
+    }
+
+
+
+
+}])
